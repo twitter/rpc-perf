757191dcaafb7ecbd3f50f4c9fec42583b91f494
Brian Martin <bmartin@twitter.com>
2019-07-31 14:30:12 -0700

    add newlines for readability

--- atomics/src/atomic_primitive/atomic_i32.rs
+++ atomics/src/atomic_primitive/atomic_i32.rs
@@ -7,26 +7,33 @@ pub struct AtomicI32 {
 
 impl AtomicPrimitive for AtomicI32 {
     type Primitive = i32;
+
     fn new(value: Self::Primitive) -> Self {
         Self {
             inner: core::sync::atomic::AtomicI32::new(value),
         }
     }
+
     fn get_mut(&mut self) -> &mut Self::Primitive {
         self.inner.get_mut()
     }
+
     fn into_inner(self) -> Self::Primitive {
         self.inner.into_inner()
     }
+
     fn load(&self, order: Ordering) -> Self::Primitive {
         self.inner.load(order)
     }
+
     fn store(&self, value: Self::Primitive, order: Ordering) {
         self.inner.store(value, order);
     }
+
     fn swap(&self, value: Self::Primitive, order: Ordering) -> Self::Primitive {
         self.inner.swap(value, order)
     }
+
     fn compare_and_swap(
         &self,
         current: Self::Primitive,
@@ -35,6 +42,7 @@ impl AtomicPrimitive for AtomicI32 {
     ) -> Self::Primitive {
         self.inner.compare_and_swap(current, new, order)
     }
+
     fn compare_exchange(
         &self,
         current: Self::Primitive,
@@ -44,6 +52,7 @@ impl AtomicPrimitive for AtomicI32 {
     ) -> Result<Self::Primitive, Self::Primitive> {
         self.inner.compare_exchange(current, new, success, failure)
     }
+
     fn compare_exchange_weak(
         &self,
         current: Self::Primitive,
