a5c5ae54b6827f3186522a89ab9807d53af16d52
Brian Martin <bmartin@twitter.com>
2020-05-22 13:09:18 -0700

    fix shellcheck errors

--- build/ci.sh
+++ build/ci.sh
@@ -17,18 +17,18 @@ fi
 
 export RUST_BACKTRACE=1
 
-if [ -z ${FEATURES} ]; then
+if [ -z "${FEATURES}" ]; then
 	cargo build
 	cargo test
 	cargo build --release
 	cargo test --release
-elif [ -n ${PACKAGE} ]; then
+elif [ -n "${PACKAGE}" ]; then
 	# features can't be enabled in the virtual manifest, cd into package
-	cd ${PACKAGE}
-	cargo build --features ${FEATURES}
-	cargo test --features ${FEATURES}
-	cargo build --release --features ${FEATURES}
-	cargo test --release --features ${FEATURES}
+	cd "${PACKAGE}"
+	cargo build --features "${FEATURES}"
+	cargo test --features "${FEATURES}"
+	cargo build --release --features "${FEATURES}"
+	cargo test --release --features "${FEATURES}"
 else
 	echo "Specified FEATURES for build, but no PACKAGE"
 	exit 1
