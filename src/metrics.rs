d3283de85b6979d2f32990449210d8493b74b0bd
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2021-09-07 16:30:57 -0700

    Add metrics definitions

--- src/metrics.rs
+++ src/metrics.rs
@@ -4,6 +4,83 @@
 
 use rustcommon_fastmetrics::{MetricsBuilder, Source};
 use strum::IntoEnumIterator;
+use rustcommon_metrics::{metric, Counter, Gauge};
+
+/// Number of connect attempts.
+#[metric(name = "connect")]
+pub static CONNECT: Counter = Counter::new();
+
+/// Number of connect errors.
+#[metric(name = "connect_ex")]
+pub static CONNECT_EX: Counter = Counter::new();
+
+/// Number of connect timeouts.
+#[metric(name = "connect_timeout")]
+pub static CONNECT_TIMEOUT: Counter = Counter::new();
+
+/// Number of requests sent.
+#[metric(name = "request")]
+pub static REQUEST: Counter = Counter::new();
+
+/// Number of request errors.
+#[metric(name = "request_ex")]
+pub static REQUEST_EX: Counter = Counter::new();
+
+/// 'get' requests for hitrate calculations.
+#[metric(name = "request_get")]
+pub static REQUEST_GET: Counter = Counter::new();
+
+/// 'ok' responses for success rate calculations
+#[metric(name = "response")]
+pub static RESPONSE: Counter = Counter::new();
+
+/// 'error' responses for success rate calculations
+#[metric(name = "response_ex")]
+pub static RESPONSE_EX: Counter = Counter::new();
+
+/// 'hit' responses for hitrate calculations
+#[metric(name = "response_hit")]
+pub static RESPONSE_HIT: Counter = Counter::new();
+
+/// Number of closed connections.
+#[metric(name = "close")]
+pub static CLOSE: Counter = Counter::new();
+
+/// Number of elapsed windows.
+#[metric(name = "window")]
+pub static WINDOW: Counter = Counter::new();
+
+/// Number of sessions.
+#[metric(name = "session")]
+pub static SESSION: Counter = Counter::new();
+
+/// Number of currently open connections.
+#[metric(name = "open")]
+pub static OPEN: Gauge = Gauge::new();
+
+/// Number of times recv has been called.
+#[metric(name = "session_recv")]
+pub static SESSION_RECV: Counter = Counter::new();
+
+/// Number of errors calling recv.
+#[metric(name = "session_recv_ex")]
+pub static SESSION_RECV_EX: Counter = Counter::new();
+
+/// Number of bytes received.
+#[metric(name = "session_recv_byte")]
+pub static SESSION_RECV_BYTE: Counter = Counter::new();
+
+/// Number of times send has been called.
+#[metric(name = "session_send")]
+pub static SESSION_SEND: Counter = Counter::new();
+
+/// Number of errors calling send.
+#[metric(name = "session_send_ex")]
+pub static SESSION_SEND_EX: Counter = Counter::new();
+
+/// Number of bytes sent.
+#[metric(name = "session_send_byte")]
+pub static SESSION_SEND_BYTE: Counter = Counter::new();
 
 use strum_macros::{AsRefStr, EnumIter};
 
