twitter/rpc-perf

commit ba73fa3fdce351240bc1344d01890ddee69d9bee

feature: add metric descriptions (#363)
Updates the metrics library and adds the new metric description
annotations for each metric. Adds corresponding descriptions into
the prometheus/opentelemetry exposition format.

src/admin.rs
@@ -256,11 +256,17 @@ impl Admin {
 
 #[derive(Clone)]
 pub struct Snapshot {
-    counters: HashMap<&'static str, u64>,
-    _gauges: HashMap<&'static str, i64>,
+    counters: HashMap<&'static str, SnapshotEntry<u64>>,
+    gauges: HashMap<&'static str, SnapshotEntry<i64>>,
     timestamp: Instant,
     connect_percentiles: Vec<(String, u64)>,
     request_percentiles: Vec<(String, u64)>,
+}
+
+#[derive(Clone)]
+pub struct SnapshotEntry<T> {
+    description: Option<&'static str>,
+    value: T,
 }
 
 impl Snapshot {
@@ -277,9 +283,17 @@ impl Snapshot {
             };
 
             if let Some(counter) = any.downcast_ref::<Counter>() {
-                counters.insert(metric.name(), counter.value());
+                let entry = SnapshotEntry {
+                    description: metric.description(),
+                    value: counter.value(),
+                };
+                counters.insert(metric.name(), entry);
             } else if let Some(gauge) = any.downcast_ref::<Gauge>() {
-                gauges.insert(metric.name(), gauge.value());
+                let entry = SnapshotEntry {
+                    description: metric.description(),
+                    value: gauge.value(),
+                };
+                gauges.insert(metric.name(), entry);
             }
         }
 
@@ -311,7 +325,7 @@ impl Snapshot {
 
         Self {
             counters,
-            _gauges: gauges,
+            gauges,
             timestamp: Instant::now(),
             connect_percentiles,
             request_percentiles,
@@ -319,8 +333,8 @@ impl Snapshot {
     }
 
     fn delta_count(&self, other: &Self, counter: &'static str) -> u64 {
-        let this = self.counters.get(&counter).unwrap_or(&0);
-        let other = other.counters.get(&counter).unwrap_or(&0);
+        let this = self.counters.get(&counter).map(|v| v.value).unwrap_or(0);
+        let other = other.counters.get(&counter).map(|v| v.value).unwrap_or(0);
         this - other
     }
 
@@ -352,14 +366,17 @@ impl Snapshot {
 
     pub fn human(&self) -> String {
         let mut data = Vec::new();
-        for (counter, value) in &self.counters {
-            data.push(format!("{}: {}", counter, value));
+        for (counter, entry) in &self.counters {
+            data.push(format!("{}: {}", counter, entry.value));
         }
-        for (label, value) in &self.connect_percentiles {
-            data.push(format!("connect_latency/{}: {}", label, value));
+        for (gauge, entry) in &self.gauges {
+            data.push(format!("{}: {}", gauge, entry.value));
         }
-        for (label, value) in &self.request_percentiles {
-            data.push(format!("response_latency/{}: {}", label, value));
+        for (label, entry) in &self.connect_percentiles {
+            data.push(format!("connect_latency/{}: {}", label, entry));
+        }
+        for (label, entry) in &self.request_percentiles {
+            data.push(format!("response_latency/{}: {}", label, entry));
         }
         data.sort();
         let mut content = data.join("\n");
@@ -371,14 +388,17 @@ impl Snapshot {
         let head = "{".to_owned();
 
         let mut data = Vec::new();
-        for (label, value) in &self.counters {
-            data.push(format!("\"{}\": {}", label, value));
+        for (label, entry) in &self.counters {
+            data.push(format!("\"{}\": {}", label, entry.value));
         }
-        for (label, value) in &self.connect_percentiles {
-            data.push(format!("\"connect_latency/{}\": {}", label, value));
+        for (label, entry) in &self.gauges {
+            data.push(format!("\"{}\": {}", label, entry.value));
         }
-        for (label, value) in &self.request_percentiles {
-            data.push(format!("\"response_latency/{}\": {}", label, value));
+        for (label, entry) in &self.connect_percentiles {
+            data.push(format!("\"connect_latency/{}\": {}", label, entry));
+        }
+        for (label, entry) in &self.request_percentiles {
+            data.push(format!("\"response_latency/{}\": {}", label, entry));
         }
         data.sort();
         let body = data.join(",");
@@ -390,21 +410,41 @@ impl Snapshot {
 
     pub fn prometheus(&self) -> String {
         let mut data = Vec::new();
-        for (counter, value) in &self.counters {
-            data.push(format!("# TYPE {} counter\n{} {}", counter, counter, value));
+        for (counter, entry) in &self.counters {
+            if let Some(description) = entry.description {
+                data.push(format!(
+                    "# HELP {} {}\n# TYPE {} counter\n{} {}",
+                    counter, description, counter, counter, entry.value
+                ));
+            } else {
+                data.push(format!(
+                    "# TYPE {} counter\n{} {}",
+                    counter, counter, entry.value
+                ));
+            }
         }
-        for (percentile, value) in &self.connect_percentiles {
+        for (gauge, entry) in &self.counters {
+            if let Some(description) = entry.description {
+                data.push(format!(
+                    "# HELP {} {}\n# TYPE {} gauge\n{} {}",
+                    gauge, description, gauge, gauge, entry.value
+                ));
+            } else {
+                data.push(format!("# TYPE {} gauge\n{} {}", gauge, gauge, entry.value));
+            }
+        }
+        for (percentile, entry) in &self.connect_percentiles {
             let label = "connect_latency";
             data.push(format!(
                 "# TYPE {} gauge\n{}{{percentile=\"{}\"}} {}",
-                label, label, percentile, value
+                label, label, percentile, entry
             ));
         }
-        for (percentile, value) in &self.request_percentiles {
+        for (percentile, entry) in &self.request_percentiles {
             let label = "response_latency";
             data.push(format!(
                 "# TYPE {} gauge\n{}{{percentile=\"{}\"}} {}",
-                label, label, percentile, value
+                label, label, percentile, entry
             ));
         }
         data.sort();

Cargo.toml
@@ -29,11 +29,11 @@ rand = { version = "0.8.0", features = ["small_rng"] }
 rand_xoshiro = { version = "0.6.0" }
 rand_distr = "0.4.0"
 rtrb = "0.2.0"
-rustcommon-heatmap = { git = "https://github.com/twitter/rustcommon", rev = "ff5ca96b31461e1b08c59df770ae17903c54c1b2" }
-rustcommon-logger = { git = "https://github.com/twitter/rustcommon", rev = "ff5ca96b31461e1b08c59df770ae17903c54c1b2" }
-rustcommon-ratelimiter = { git = "https://github.com/twitter/rustcommon", rev = "ff5ca96b31461e1b08c59df770ae17903c54c1b2" }
-rustcommon-metrics = { git = "https://github.com/twitter/rustcommon", rev = "ff5ca96b31461e1b08c59df770ae17903c54c1b2", features = ["heatmap"] }
-rustcommon-time = { git = "https://github.com/twitter/rustcommon", rev = "ff5ca96b31461e1b08c59df770ae17903c54c1b2" }
+rustcommon-heatmap = { git = "https://github.com/twitter/rustcommon", rev = "5bf3c0e" }
+rustcommon-logger = { git = "https://github.com/twitter/rustcommon", rev = "5bf3c0e" }
+rustcommon-ratelimiter = { git = "https://github.com/twitter/rustcommon", rev = "5bf3c0e" }
+rustcommon-metrics = { git = "https://github.com/twitter/rustcommon", rev = "5bf3c0e", features = ["heatmap"] }
+rustcommon-time = { git = "https://github.com/twitter/rustcommon", rev = "5bf3c0e" }
 serde = "1.0.130"
 serde_derive = "1.0.130"
 serde_json = "1.0.72"

src/metrics.rs
@@ -5,40 +5,37 @@
 use rustcommon_metrics::metric;
 pub use rustcommon_metrics::{Counter, Gauge};
 
-/// Number of connect attempts.
-#[metric(name = "connect")]
+#[metric(name = "connect", description = "connect attempts")]
 pub static CONNECT: Counter = Counter::new();
 
-/// Number of connect errors.
-#[metric(name = "connect_ex")]
+#[metric(name = "connect_ex", description = "exceptions when calling connect")]
 pub static CONNECT_EX: Counter = Counter::new();
 
-/// Number of connect timeouts.
-#[metric(name = "connect_timeout")]
+#[metric(name = "connect_timeout", description = "connect timeouts")]
 pub static CONNECT_TIMEOUT: Counter = Counter::new();
 
-/// Number of requests sent.
-#[metric(name = "request")]
+#[metric(name = "request", description = "requests sent")]
 pub static REQUEST: Counter = Counter::new();
 
-/// Number of request errors.
-#[metric(name = "request_ex")]
+#[metric(name = "request_ex", description = "exceptions when sending a request")]
 pub static REQUEST_EX: Counter = Counter::new();
 
-/// 'get' requests for hitrate calculations.
-#[metric(name = "request_get")]
+#[metric(name = "request_get", description = "get requests sent")]
 pub static REQUEST_GET: Counter = Counter::new();
 
-/// 'ok' responses for success rate calculations
-#[metric(name = "response")]
+#[metric(name = "response", description = "responses received")]
 pub static RESPONSE: Counter = Counter::new();
 
-/// 'error' responses for success rate calculations
-#[metric(name = "response_ex")]
+#[metric(
+    name = "response_ex",
+    description = "responses that indicated an error"
+)]
 pub static RESPONSE_EX: Counter = Counter::new();
 
-/// 'hit' responses for hitrate calculations
-#[metric(name = "response_hit")]
+#[metric(
+    name = "response_hit",
+    description = "responses that indicated a cache hit"
+)]
 pub static RESPONSE_HIT: Counter = Counter::new();
 
 /// distribution of response latencies
@@ -47,46 +44,50 @@ pub static RESPONSE_HIT: Counter = Counter::new();
 //     Heatmap::new(1_000_000_000, 3, Duration::from_secs(60), Duration::from_secs(1))
 // );
 
-/// Number of closed connections.
-#[metric(name = "close")]
+#[metric(name = "close", description = "closed connections")]
 pub static CLOSE: Counter = Counter::new();
 
-/// Number of elapsed windows.
-#[metric(name = "window")]
+#[metric(name = "window", description = "elapsed windows")]
 pub static WINDOW: Counter = Counter::new();
 
-/// Number of sessions.
-#[metric(name = "session")]
+#[metric(name = "session", description = "sessions created")]
 pub static SESSION: Counter = Counter::new();
 
-/// Number of currently open connections.
-#[metric(name = "open")]
+#[metric(name = "open", description = "open connections")]
 pub static OPEN: Gauge = Gauge::new();
 
-/// Number of times recv has been called.
-#[metric(name = "session_recv")]
+#[metric(name = "session_recv", description = "session receive attempts")]
 pub static SESSION_RECV: Counter = Counter::new();
 
-/// Number of errors calling recv.
-#[metric(name = "session_recv_ex")]
+#[metric(
+    name = "session_recv_ex",
+    description = "exceptions when calling receive on session"
+)]
 pub static SESSION_RECV_EX: Counter = Counter::new();
 
-/// Number of bytes received.
-#[metric(name = "session_recv_byte")]
+#[metric(
+    name = "session_recv_byte",
+    description = "bytes received for all sessions"
+)]
 pub static SESSION_RECV_BYTE: Counter = Counter::new();
 
-/// Number of times send has been called.
-#[metric(name = "session_send")]
+#[metric(name = "session_send", description = "session send attempts")]
 pub static SESSION_SEND: Counter = Counter::new();
 
-/// Number of errors calling send.
-#[metric(name = "session_send_ex")]
+#[metric(
+    name = "session_send_ex",
+    description = "execptions when calling send on session"
+)]
 pub static SESSION_SEND_EX: Counter = Counter::new();
 
-/// Number of bytes sent.
-#[metric(name = "session_send_byte")]
+#[metric(
+    name = "session_send_byte",
+    description = "bytes sent for all sessions"
+)]
 pub static SESSION_SEND_BYTE: Counter = Counter::new();
 
-/// Number of sessions which were reused.
-#[metric(name = "session_reuse")]
+#[metric(
+    name = "session_reuse",
+    description = "session reused with abbreviated TLS handshake"
+)]
 pub static SESSION_REUSE: Counter = Counter::new();

commit 0aaa720de5f45c4cce02ecd979a0e332117403f3

fix: fix issue introduced in #363
PR #363 added metrics descriptions, but mistakenly iterates through
the counters twice for prometheus/opentelemetry format and
incorrectly reports each counter as both a counter and a gauge.

Fixes a bad copy-paste from that PR.

src/admin.rs
@@ -423,7 +423,7 @@ impl Snapshot {
                 ));
             }
         }
-        for (gauge, entry) in &self.counters {
+        for (gauge, entry) in &self.gauges {
             if let Some(description) = entry.description {
                 data.push(format!(
                     "# HELP {} {}\n# TYPE {} gauge\n{} {}",